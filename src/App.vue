cc9c0970f6ba3933fee79af19e73adebc9aae378
Aziz Salimli <azizsalimoglu02@gmail.com>
2022-09-27 14:00:40 +0400

    Category input added

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,9 @@ const todos_asc = computed(() => todos.value.sort((a, b) => {
   return b.createdAt - a.createdAt
 }))
 
+const addTodo = () => {
+}
+
 watch(name, (newVal) => {
   localStorage.setItem('name', newVal)
 })
@@ -29,6 +32,28 @@ onMounted(() => {
       </h2>
     </section>
 
+    <section class="create-todo">
+      <h3>CREATE A TODO</h3>
+      <form @submit.prevent="addTodo">
+        <h4>What's on your todo list?</h4>
+        <input type="text" placeholder="example todo" v-model="input_content">
+        <h4>Pick a category</h4>
+
+        <div class="options">
+          <label>
+            <input type="radio" name="category" id="category1" value="business" v-model="input_category">
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+
+          <label>
+            <input type="radio" name="category" id="category2" value="personal" v-model="input_category">
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+        </div>
+      </form>
+    </section>
   </main>
 </template>
 
